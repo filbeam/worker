db73fa53051a2d9c180d95e62f54e80e6a407080
Srdjan S <stankovic.srdjo@gmail.com>
2025-10-07 16:03:57 +0200

    chore: clean migration file

--- db/migrations/0018_add_egress_quota_columns.sql
+++ db/migrations/0018_add_egress_quota_columns.sql
@@ -1,5 +1,3 @@
--- Migration number: 0018 	 2025-01-06T00:00:00.000Z
--- Add egress quota columns to data_sets table
--- Using TEXT to store large numbers since uint256 can exceed SQLite INTEGER limits
+-- Migration number: 0018 	 2025-10-06T00:00:00.000Z
 ALTER TABLE data_sets ADD COLUMN cdn_egress_quota TEXT;
 ALTER TABLE data_sets ADD COLUMN cache_miss_egress_quota TEXT;
