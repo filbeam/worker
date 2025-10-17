e19e333e228467a80cc646d877ba2178a83f7955
Srdjan S <stankovic.srdjo@gmail.com>
2025-10-17 14:00:26 +0200

    chore: remove unused timestampMsToEpoch

--- usage-reporter/lib/epoch.js
+++ usage-reporter/lib/epoch.js
@@ -9,17 +9,3 @@
 export function epochToTimestampMs(epoch, genesisBlockTimestampMs) {
   return Number(epoch) * 30 * 1000 + Number(genesisBlockTimestampMs)
 }
-/**
- * Convert timestamp in milliseconds to Filecoin epoch
- *
- * @param {number} timestampMs - Timestamp in milliseconds
- * @param {bigint} genesisBlockTimestampMs - Genesis block timestamp in
- *   milliseconds
- * @returns {number} Filecoin epoch number
- */
-
-export function timestampMsToEpoch(timestampMs, genesisBlockTimestampMs) {
-  return Math.floor(
-    (timestampMs - Number(genesisBlockTimestampMs)) / (30 * 1000),
-  )
-}
