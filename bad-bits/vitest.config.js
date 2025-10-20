9107a5d0dee812041ed78cd1aa4e7aca2e253d4b
Srdjan S <stankovic.srdjo@gmail.com>
2025-10-20 12:06:50 +0200

    fix: remove apply-migrations.js reference from vitest.config.js

--- bad-bits/vitest.config.js
+++ bad-bits/vitest.config.js
@@ -3,7 +3,6 @@ import { defineWorkersProject } from '@cloudflare/vitest-pool-workers/config'
 export default defineWorkersProject(async () => {
   return {
     test: {
-      setupFiles: ['./test/apply-migrations.js'],
       poolOptions: {
         workers: {
           singleWorker: true,
