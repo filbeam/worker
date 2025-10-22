25957eff1da409469398aaeeddfc3c3d343dff89
Julian Gruber <julian@juliangruber.com>
2025-10-22 16:01:26 +0200

    Move bad bits to kv (#367)
    * move bad bits to kv
    
    * chore: fix TypeScript errors
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    * fix: remove apply-migrations.js reference from vitest.config.js
    
    * move `getAllBadBitHashes`
    
    * only write etag if was capped
    
    * give bad bits its own kv namespace
    
    * refactor `persistUpdates()`
    
    * allow `latest-hashes:*` to shrink
    
    * Apply suggestion from @pyropy
    
    Co-authored-by: Srdjan <stankovic.srdjo@gmail.com>
    
    * Apply suggestion from @pyropy
    
    Co-authored-by: Srdjan <stankovic.srdjo@gmail.com>
    
    * Apply suggestion from @pyropy
    
    Co-authored-by: Srdjan <stankovic.srdjo@gmail.com>
    
    * refactor using `Set#difference()`
    
    * fmt
    
    * refactor
    
    * test: reliably clear all kv data
    
    * fix `wasCapped`
    
    * refactor
    
    * Update bad-bits/lib/store.js
    
    Co-authored-by: Miroslav Bajtoš <oss@bajtos.net>
    
    * use r2 for `latest-hashes`
    
    * fix test
    
    ---------
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    Co-authored-by: Miroslav Bajtoš <oss@bajtos.net>
    Co-authored-by: Srdjan S <stankovic.srdjo@gmail.com>

--- bad-bits/lib/store.js
+++ bad-bits/lib/store.js
@@ -1,58 +1,103 @@
+const MAX_TOTAL_CHANGES = 10_000
+const MAX_KV_BATCH_SIZE = 1_000
+
 /**
  * Updates the bad bits database with new hashes
  *
- * @param {Env} env - Environment containing database connection
+ * @param {{ BAD_BITS_KV: KVNamespace; BAD_BITS_R2: R2Bucket }} env -
+ *   Environment containing database connection
  * @param {Set<string>} currentHashes - Set of current valid hashes from
  *   denylist
  * @param {string | null} etag - ETag for the current denylist
  */
 export async function updateBadBitsDatabase(env, currentHashes, etag) {
   const startedAt = Date.now()
-  const timestamp = new Date().toISOString()
-  const insertBadBitStmt = env.DB.prepare(
-    `
-    INSERT INTO bad_bits (hash, last_modified_at) VALUES (?, ?)
-    ON CONFLICT(hash) DO UPDATE SET last_modified_at = excluded.last_modified_at
-    `,
+
+  console.log('getting latest hashes')
+  const oldHashes = new Set(await getAllBadBitHashes(env))
+
+  console.log('comparing current hashes')
+  let wasCapped = false
+  const addedHashes = [...currentHashes.difference(oldHashes)]
+  const removedHashes = [...oldHashes.difference(currentHashes)]
+  if (addedHashes.length > MAX_TOTAL_CHANGES) {
+    addedHashes.splice(MAX_TOTAL_CHANGES)
+    wasCapped = true
+  }
+  if (removedHashes.length > MAX_TOTAL_CHANGES - addedHashes.length) {
+    removedHashes.splice(MAX_TOTAL_CHANGES - addedHashes.length)
+    wasCapped = true
+  }
+
+  await persistUpdates(env, oldHashes, addedHashes, removedHashes)
+
+  if (etag && !wasCapped) {
+    await env.BAD_BITS_KV.put('latest-etag', etag)
+  }
+
+  console.log(
+    `+${addedHashes.length} -${removedHashes.length} hashes in ${Date.now() - startedAt}ms`,
   )
-
-  let updated = 0
-  const remainingHashes = Array.from(currentHashes)
-  while (remainingHashes.length > 0) {
-    // pop first N hashes from remainingHashes
-    const batchHashes = remainingHashes.splice(0, 10_000)
-
-    await env.DB.batch(
-      batchHashes.map((hash) => insertBadBitStmt.bind(hash, timestamp)),
-    )
-
-    updated += batchHashes.length
-    console.log(
-      `Inserted/updated ${updated} bad bits in ${Date.now() - startedAt}ms`,
-    )
-  }
-
-  const statements = [
-    env.DB.prepare('DELETE FROM bad_bits WHERE last_modified_at < ?').bind(
-      timestamp,
-    ),
-  ]
-
-  if (etag) {
-    statements.push(
-      env.DB.prepare(
-        'INSERT INTO bad_bits_history (timestamp, etag) VALUES (?, ?)',
-      ).bind(timestamp, etag),
-    )
-  }
-
-  await env.DB.batch(statements)
 }
 
-/** @param {Env} env */
+/** @param {{ BAD_BITS_KV: KVNamespace }} env */
 export async function getLastEtag(env) {
-  const result = await env.DB.prepare(
-    'SELECT etag FROM bad_bits_history ORDER BY timestamp DESC LIMIT 1',
-  ).first()
-  return result ? /** @type {string} */ (result.etag) : null
+  return await env.BAD_BITS_KV.get('latest-etag')
+}
+
+/**
+ * Gets all bad bit hashes from the database
+ *
+ * @param {{ BAD_BITS_R2: R2Bucket }} env - Environment containing database
+ *   connection
+ * @returns {Promise<string[]>} - Array of hash strings
+ */
+export async function getAllBadBitHashes(env) {
+  const object = await env.BAD_BITS_R2.get('latest-hashes')
+  return object ? await object.json() : []
+}
+
+/**
+ * @param {{ BAD_BITS_KV: KVNamespace; BAD_BITS_R2: R2Bucket }} env
+ * @param {Set<string>} oldHashes
+ * @param {string[]} addedHashes
+ * @param {string[]} removedHashes
+ * @returns {Promise<void>}
+ */
+async function persistUpdates(env, oldHashes, addedHashes, removedHashes) {
+  console.log('writing added hashes')
+  for (let i = 0; i < Math.ceil(addedHashes.length / MAX_KV_BATCH_SIZE); i++) {
+    const batch = addedHashes.slice(
+      i * MAX_KV_BATCH_SIZE,
+      (i + 1) * MAX_KV_BATCH_SIZE,
+    )
+    await Promise.all(
+      batch.map((hash) => env.BAD_BITS_KV.put(`bad-bits:${hash}`, 'true')),
+    )
+  }
+
+  console.log('deleting removed hashes')
+  for (
+    let i = 0;
+    i < Math.ceil(removedHashes.length / MAX_KV_BATCH_SIZE);
+    i++
+  ) {
+    const batch = removedHashes.slice(
+      i * MAX_KV_BATCH_SIZE,
+      (i + 1) * MAX_KV_BATCH_SIZE,
+    )
+    await Promise.all(
+      batch.map((hash) => env.BAD_BITS_KV.delete(`bad-bits:${hash}`)),
+    )
+  }
+
+  console.log('storing latest hashes')
+  const latestHashes = new Set(oldHashes)
+  for (const hash of addedHashes) {
+    latestHashes.add(hash)
+  }
+  for (const hash of removedHashes) {
+    latestHashes.delete(hash)
+  }
+  await env.BAD_BITS_R2.put('latest-hashes', JSON.stringify([...latestHashes]))
 }
