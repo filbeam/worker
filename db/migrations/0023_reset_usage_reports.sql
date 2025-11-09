5fe1dac8ad326e866be8cb76e5156a6ac0ebc64e
Srdjan S <stankovic.srdjo@gmail.com>
2025-11-09 12:24:34 +0100

    fix: remove where clause from reset query

--- db/migrations/0023_reset_usage_reports.sql
+++ db/migrations/0023_reset_usage_reports.sql
@@ -4,6 +4,4 @@ UPDATE
     data_sets
 SET 
     usage_reported_until = '1970-01-01T00:00:00.000Z',
-    pending_usage_report_tx_hash = NULL
-WHERE 
-    pending_usage_report_tx_hash IS NOT NULL;
+    pending_usage_report_tx_hash = NULL;
