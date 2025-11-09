3c322f1306f62eccf3774566f65d2733af02f80b
Srdjan S <stankovic.srdjo@gmail.com>
2025-11-09 11:31:45 +0100

    fix: add migration to force full report to FilBeamOperator v1.0.2

---
+++ db/migrations/0023_reset_usage_reports.sql
@@ -0,0 +1,9 @@
+-- Reset existing usage reports due to migration from FilBeamOperator v1.0.1 to v1.0.2 
+-- We are not able to settle usage reported to the FilBeamOperator v1.0.1 so we should re-report full usage
+UPDATE 
+    data_sets
+SET 
+    usage_reported_until = '1970-01-01T00:00:00.000Z',
+    pending_usage_report_tx_hash = NULL
+WHERE 
+    pending_usage_report_tx_hash IS NOT NULL;
