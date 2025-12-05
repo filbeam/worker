c35c424b017f26809c898077cecf000e26df3136
Julian Gruber <julian@juliangruber.com>
2025-12-05 13:52:02 +0100

    add matching log

--- piece-retriever/bin/piece-retriever.js
+++ piece-retriever/bin/piece-retriever.js
@@ -118,6 +118,7 @@ export default {
             { signal: request.signal },
           )
           if (retrievalResult.response.ok) {
+            console.log('Retrieval attempt succeeded')
             break
           }
           console.log(
